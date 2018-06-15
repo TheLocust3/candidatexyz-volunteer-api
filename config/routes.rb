aeb08601f47538f71391b8f91fb1703f09f1563a
TheLocust3 <jake.kinsella@gmail.com>
2018-06-15 16:57:43 -0400

    Small bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'volunteers/number_of_pages' => 'volunteers#get_number_of_pages'
   resources :volunteers
 
+  delete 'contacts/unsubscribe' => 'contacts#unsubscribe'
   resources :contacts
   
   resources :messages
