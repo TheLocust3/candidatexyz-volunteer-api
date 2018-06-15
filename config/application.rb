a48b34d875bb8f96c75bde86a77ea357ba96c078
TheLocust3 <jake.kinsella@gmail.com>
2018-06-15 16:14:12 -0400

    Random cleanups

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -9,7 +8,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
@@ -18,15 +16,15 @@ module VolunteerApi
   class Application < Rails::Application
     config.load_defaults 5.1
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'https://candidatexyz.com'
-        resource '*', headers: :any, methods: [:get, :post, :options]
-      end
-    end
+    config.api_only = true
 
     Jbuilder.key_format camelize: :lower
 
-    config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors, debug: true do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [ :get, :options ]
+      end
+    end
   end
 end
