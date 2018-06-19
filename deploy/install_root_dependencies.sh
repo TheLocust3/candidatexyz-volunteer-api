b9d389fcca574ae0164b1c89f7fb42701b407528
TheLocust3 <jake.kinsella@gmail.com>
2018-06-19 14:44:28 -0400

    Update root dependencies

--- deploy/install_root_dependencies.sh
+++ deploy/install_root_dependencies.sh
@@ -6,6 +6,8 @@ cd /home/ubuntu
 
 sudo apt-get update
 
+sudo apt-get install libpq-dev # pg gem dependency
+
 # install updated version of ruby build
 sudo apt-get install -y rbenv ruby-build autoconf bison
 mkdir -p ~/.rbenv/plugins
