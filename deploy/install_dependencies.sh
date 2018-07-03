7ee2cfa230c731629b68c043bf95a359e580cc01
TheLocust3 <jake.kinsella@gmail.com>
2018-07-02 23:53:01 -0400

    Fix deployments

--- deploy/install_dependencies.sh
+++ deploy/install_dependencies.sh
@@ -32,7 +32,7 @@ cd /home/ubuntu/rails
 
 sudo apt-get install -y awscli
 
-aws s3 cp s3://candidatexyz-volunteerapi/common.tar.gz /home/ubuntu/rails/
+aws s3 cp s3://$BUCKET/common.tar.gz /home/ubuntu/rails/
 tar -xvzf common.tar.gz
 
 # pull secrets
