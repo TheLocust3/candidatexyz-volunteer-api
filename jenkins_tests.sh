3478d648f3e8cf469de31fb4ed262dff6822b26f
TheLocust3 <jake.kinsella@gmail.com>
2018-08-08 15:51:42 -0400

    Use credentials in ssh key

--- jenkins_tests.sh
+++ jenkins_tests.sh
@@ -2,7 +2,17 @@
 
 set -e
 
+echo $SSH_KEY > key
+
+path=pwd
+
+rm /home/ubuntu/.ssh/config
+echo "Host github.com
+  IdentityFile ${path}/key" > /home/ubuntu/.ssh/config
+
 gem install bundler
 bundle install
 
 bundle exec rails test
+
+rm key
