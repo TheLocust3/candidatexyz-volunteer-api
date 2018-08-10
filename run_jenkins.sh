5ee3128b26a0fbe90689e823dc7309fa9e653c6c
TheLocust3 <jake.kinsella@gmail.com>
2018-08-10 14:31:12 -0400

    Hopefully got jenkins to work

--- run_jenkins.sh
+++ run_jenkins.sh
@@ -22,7 +22,7 @@ bundle exec rake db:create
 bundle exec rake db:schema:load
 bundle exec rake db:seed
 
-# bundle exec puma -b tcp://127.0.0.1:3003 &
+bundle exec puma -b tcp://127.0.0.1:3003 &
 
 cd ../
 
@@ -35,4 +35,4 @@ bundle exec rake db:seed
 
 echo Running tests
 
-./bin/rails test
+bundle exec rails test
