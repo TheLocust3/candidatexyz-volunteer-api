56f6e79da299d21bdbdb87d9805be6aa13eaf975
TheLocust3 <jake.kinsella@gmail.com>
2018-08-10 11:44:40 -0400

    Save gems in cached folder

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -4,17 +4,17 @@ set -e
 
 mkdir ~/.bundle
 touch ~/.bundle/config
-echo BUNDLE_PATH: vendor/bundle > ~/.bundle/config
+echo BUNDLE_PATH: ~/.bundle/gems > ~/.bundle/config
 
 gem install bundler
 
 cd common
-bundle install --path vendor/bundle
+bundle install --path ~/.bundle/gems
 
 cd ../user-api/
 
 bundle config --local local.candidatexyz-common ../common/
-bundle install --path vendor/bundle
+bundle install --path ~/.bundle/gems
 
 bundle exec rake db:create
 bundle exec rake db:schema:load
@@ -25,7 +25,7 @@ bundle exec puma -b tcp://127.0.0.1:3003 &
 cd ../
 
 bundle config --local local.candidatexyz-common common/
-bundle install --path vendor/bundle
+bundle install --path ~/.bundle/gems
 
 bundle exec rake db:create
 bundle exec rake db:schema:load
